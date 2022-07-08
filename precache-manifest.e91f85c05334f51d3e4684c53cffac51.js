c03111a64d197df0017fecfe65300b823a4eca82
DESKTOP-P7P3MBD\Admin <vinod.patil377@gmail.com>
2022-07-08 20:25:14 +0530

    Updates

---
+++ precache-manifest.e91f85c05334f51d3e4684c53cffac51.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "441c6a107f80cdfbf666",
+    "url": "/paint_company/static/css/main.77b1dcb3.chunk.css"
+  },
+  {
+    "revision": "441c6a107f80cdfbf666",
+    "url": "/paint_company/static/js/main.441c6a10.chunk.js"
+  },
+  {
+    "revision": "1af302f4bdedd31671fc",
+    "url": "/paint_company/static/js/1.1af302f4.chunk.js"
+  },
+  {
+    "revision": "ce8b15fc3390e2a739a6",
+    "url": "/paint_company/static/js/runtime~main.ce8b15fc.js"
+  },
+  {
+    "revision": "6c1a5568512484910e6dd722e76c3a1a",
+    "url": "/paint_company/static/media/cont.6c1a5568.jpg"
+  },
+  {
+    "revision": "f0b492f70ed7af728f5c514681292528",
+    "url": "/paint_company/static/media/about.f0b492f7.png"
+  },
+  {
+    "revision": "6b1ca5d246f8989617a075d6ad817242",
+    "url": "/paint_company/static/media/work1.6b1ca5d2.png"
+  },
+  {
+    "revision": "137fb741d8ad0f0165b98594f9656faa",
+    "url": "/paint_company/static/media/work2.137fb741.png"
+  },
+  {
+    "revision": "e254f824107e48a923887bddf77d168d",
+    "url": "/paint_company/static/media/work3.e254f824.png"
+  },
+  {
+    "revision": "8c376e4a43154f92a18fb62baf593773",
+    "url": "/paint_company/static/media/hero.8c376e4a.png"
+  },
+  {
+    "revision": "665dd9688e546e13edc9a4fa26e73d5d",
+    "url": "/paint_company/static/media/ctl.665dd968.png"
+  },
+  {
+    "revision": "c6e06ca0f24645e122c82109fa9e5cf8",
+    "url": "/paint_company/static/media/callaction.c6e06ca0.png"
+  },
+  {
+    "revision": "6b5b999caec5686eeb339a609ca1373c",
+    "url": "/paint_company/index.html"
+  }
+];
